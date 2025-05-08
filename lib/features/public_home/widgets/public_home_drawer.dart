0386e4540336cc43f67b2869ed252d76c27a2758
Newton-Mitro <newtonmitro@gmail.com>
2025-05-08 23:02:47 +0600

    drawer fixed

--- lib/features/public_home/widgets/public_home_drawer.dart
+++ lib/features/public_home/widgets/public_home_drawer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pashboi/core/constants/app_images.dart';
+import 'package:pashboi/core/extensions/app_context.dart';
 
 class PublicHomeDrawer extends StatelessWidget {
   const PublicHomeDrawer({super.key});
@@ -9,54 +12,111 @@ class PublicHomeDrawer extends StatelessWidget {
       child: Column(
         children: [
           UserAccountsDrawerHeader(
-            accountName: Text("John Doe"),
-            accountEmail: Text("john.doe@example.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/logo.png"),
+            accountName: Text(
+              "Dhaka Credit",
+              style: TextStyle(
+                fontSize: 16,
+                color: context.theme.colorScheme.onPrimary,
+              ),
             ),
-            // decoration: BoxDecoration(color: Colors.blueGrey),
+            accountEmail: Text(
+              "Employment Creation is Our Goal; \nSelf-Reliant Community is Our Dream.",
+              style: TextStyle(
+                fontSize: 12,
+                color: context.theme.colorScheme.onPrimary,
+              ),
+            ),
+            currentAccountPicture: Image.asset(
+              AppImages.pathLogo,
+              color: Colors.white,
+            ),
           ),
           ListTile(
-            leading: Icon(Icons.stacked_line_chart_outlined),
-            title: Text("Info"),
+            leading: const FaIcon(FontAwesomeIcons.circleInfo, size: 16),
+            title: Text(
+              "About Us",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.monetization_on_sharp),
-            title: Text("Accounts"),
+            leading: const FaIcon(FontAwesomeIcons.userTie, size: 16),
+            title: Text(
+              "Founders Profile",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.car_rental),
-            title: Text("Loans"),
+            leading: const FaIcon(FontAwesomeIcons.bullseye, size: 16),
+            title: Text(
+              "Mission and Vision",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.safety_check),
-            title: Text("Surety"),
+            leading: const FaIcon(FontAwesomeIcons.userShield, size: 16),
+            title: Text(
+              "Privacy Policy",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.savings),
-            title: Text("Deposit"),
+            leading: const FaIcon(FontAwesomeIcons.code, size: 16),
+            title: Text(
+              "Development Credits",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
           ListTile(
-            leading: Icon(Icons.atm),
-            title: Text("Withdraw"),
+            leading: const FaIcon(FontAwesomeIcons.bolt, size: 16),
+            title: Text(
+              "Powered By",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
+          const Spacer(),
           ListTile(
-            leading: Icon(Icons.account_balance),
-            title: Text("Transfer"),
+            leading: const FaIcon(FontAwesomeIcons.houseChimney, size: 16),
+            title: Text(
+              "Home",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: context.theme.colorScheme.onSurface,
+              ),
+            ),
             onTap: () {},
           ),
-          Spacer(),
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text("Logout"),
-            onTap: () {},
-          ),
+          const SizedBox(height: 10),
         ],
       ),
     );
