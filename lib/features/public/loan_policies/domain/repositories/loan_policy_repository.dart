01a18974241d4b89019f2c1be867395e7f6056b6
israfil <6347israfil@gmail.com>
2025-07-01 15:37:58 +0600

    update

--- lib/features/public/loan_policies/domain/repositories/loan_policy_repository.dart
+++ lib/features/public/loan_policies/domain/repositories/loan_policy_repository.dart
@@ -1,10 +1,9 @@
 import 'package:pashboi/core/types/typedef.dart';
-import 'package:pashboi/features/public/deposit_policies/domain/enities/deposit_policy_entity.dart';
-import 'package:pashboi/features/public/deposit_policies/domain/usecases/fetch_deposit_policy_usecase.dart';
 import 'package:pashboi/features/public/loan_policies/domain/entites/loan_policy_entity.dart';
+import 'package:pashboi/features/public/loan_policies/domain/usecases/fetch_loant_policy_usecase.dart';
 
 abstract class LoanPolicyRepository {
-  ResultFuture<List<LoanPolicyEntity>> findDepositPoliciesByCategoryId(
-    FetchDepositPllicyProps props,
+  ResultFuture<List<LoanPolicyEntity>> findLoanPoliciesByCategoryId(
+    FetchLoanPolicyProps props,
   );
 }
---
+++ lib/features/public/loan_policies/domain/usecases/fetch_loant_policy_usecase.dart
@@ -0,0 +1,24 @@
+import 'package:pashboi/core/types/typedef.dart';
+import 'package:pashboi/core/usecases/usecase.dart';
+import 'package:pashboi/features/public/deposit_policies/domain/enities/deposit_policy_entity.dart';
+import 'package:pashboi/features/public/deposit_policies/domain/repositories/deposit_policy_repository.dart';
+import 'package:pashboi/features/public/loan_policies/domain/entites/loan_policy_entity.dart';
+import 'package:pashboi/features/public/loan_policies/domain/repositories/loan_policy_repository.dart';
+
+class FetchLoanPolicyProps {
+  final String categoryId;
+
+  FetchLoanPolicyProps({required this.categoryId});
+}
+
+class FetchLoanPolicyUseCase
+    extends UseCase<List<LoanPolicyEntity>, FetchLoanPolicyProps> {
+  final LoanPolicyRepository loanPolicyRepository;
+
+  FetchLoanPolicyUseCase({required this.loanPolicyRepository});
+
+  @override
+  ResultFuture<List<LoanPolicyEntity>> call(FetchLoanPolicyProps props) async {
+    return loanPolicyRepository.findLoanPoliciesByCategoryId(props);
+  }
+}
