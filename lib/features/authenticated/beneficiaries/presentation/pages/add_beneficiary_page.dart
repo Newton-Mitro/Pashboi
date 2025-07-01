49305bfe7a776d20c3b981cf5ccd9d6bbb206ed4
newton-mitro <newtonmitro@gmail.com>
2025-07-01 18:32:59 +0600

    update

--- lib/features/authenticated/beneficiaries/presentation/pages/add_beneficiary_page.dart
+++ lib/features/authenticated/beneficiaries/presentation/pages/add_beneficiary_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pashboi/core/extensions/app_context.dart';
+import 'package:pashboi/features/authenticated/beneficiaries/presentation/pages/bloc/beneficiary_bloc.dart';
 import 'package:pashboi/shared/widgets/app_search_input.dart';
 import 'package:pashboi/shared/widgets/app_text_input.dart';
 import 'package:pashboi/shared/widgets/page_container.dart';
@@ -18,72 +20,108 @@ class _AddBeneficiaryPageState extends State<AddBeneficiaryPage> {
       TextEditingController();
   final TextEditingController _accountHolderController =
       TextEditingController();
+
+  bool _isValidInput() {
+    return _accountSearchController.text.isNotEmpty &&
+        _accountHolderController.text.isNotEmpty;
+  }
+
+  void _submit() {
+    if (!_isValidInput()) {
+      if (!mounted) return;
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Please enter all fields")));
+      return;
+    }
+
+    if (!mounted) return;
+    context.read<BeneficiaryBloc>().add(
+      CreateBeneficiary(
+        accountNumber: _accountSearchController.text.trim(),
+        beneficiaryName: _accountHolderController.text.trim(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(title: const Text('Add Beneficiary')),
-      body: PageContainer(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(
-                  FontAwesomeIcons.users,
-                  size: 40,
-                  color: context.theme.colorScheme.onSurface,
-                ),
-                const SizedBox(height: 4),
-                const Align(
-                  alignment: Alignment.center,
-                  child: Text(
+
+    return BlocListener<BeneficiaryBloc, BeneficiaryState>(
+      listener: (context, state) {
+        if (state is BeneficiaryError) {
+          if (!mounted) return;
+          ScaffoldMessenger.of(
+            context,
+          ).showSnackBar(SnackBar(content: Text(state.error)));
+        }
+
+        if (state is BeneficiaryLoaded) {
+          if (!mounted) return;
+          Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Add Beneficiary')),
+        body: PageContainer(
+          child: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    FontAwesomeIcons.users,
+                    size: 40,
+                    color: context.theme.colorScheme.onSurface,
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
                     "Add Beneficiary",
                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                   ),
-                ),
-                const SizedBox(height: 45),
-                AppSearchTextInput(
-                  controller: _accountSearchController,
-                  label: "Account Number",
-                  isSearch: true,
-                  prefixIcon: Icon(
-                    FontAwesomeIcons.piggyBank,
-                    color: context.theme.colorScheme.onSurface,
+                  const SizedBox(height: 45),
+                  AppSearchTextInput(
+                    controller: _accountSearchController,
+                    label: "Account Number",
+                    isSearch: true,
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.piggyBank,
+                      color: context.theme.colorScheme.onSurface,
+                    ),
+                    onSearchPressed: () {
+                      debugPrint(
+                        "User searched: ${_accountSearchController.text}",
+                      );
+                    },
                   ),
-                  onSearchPressed: () {
-                    print("User searched: ${_accountSearchController.text}");
-                  },
-                ),
-                const SizedBox(height: 16),
-                AppTextInput(
-                  controller: _accountHolderController,
-                  label: "Account Holder Name",
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: context.theme.colorScheme.onSurface,
+                  const SizedBox(height: 16),
+                  AppTextInput(
+                    controller: _accountHolderController,
+                    label: "Account Holder Name",
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: context.theme.colorScheme.onSurface,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(5),
-        child: ProgressSubmitButton(
-          width: width - 10,
-          height: 100,
-          backgroundColor: context.theme.colorScheme.primary,
-          progressColor: context.theme.colorScheme.secondary,
-          foregroundColor: context.theme.colorScheme.onPrimary,
-          duration: 3,
-          label: 'Hold & Press to Submit',
-          onSubmit: () {
-            // Final submission logic
-          },
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.all(5),
+          child: ProgressSubmitButton(
+            width: width - 10,
+            height: 100,
+            backgroundColor: context.theme.colorScheme.primary,
+            progressColor: context.theme.colorScheme.secondary,
+            foregroundColor: context.theme.colorScheme.onPrimary,
+            duration: 3,
+            label: 'Hold & Press to Submit',
+            onSubmit: _submit,
+          ),
         ),
       ),
     );
