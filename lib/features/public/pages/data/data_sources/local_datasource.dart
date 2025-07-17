c81858ba01f65b8b5f5aa32123a654f832e0f7f9
israfil <6347israfil@gmail.com>
2025-07-17 17:16:48 +0600

    update page loacal Datasource

--- lib/features/public/pages/data/data_sources/local_datasource.dart
+++ lib/features/public/pages/data/data_sources/local_datasource.dart
@@ -1,1 +1,37 @@
+import 'dart:convert';
+import 'package:pashboi/features/public/pages/data/models/page_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+/// Interface for local page data source
+abstract class PageLocalDataSource {
+  Future<PageModel?> fetchPage(String slug);
+  Future<void> storePage(PageModel page, String slug);
+}
+
+/// Implementation using SharedPreferences
+class PageLocalDataSourceImpl implements PageLocalDataSource {
+  final SharedPreferences _sharedPreferences;
+
+  PageLocalDataSourceImpl(this._sharedPreferences);
+
+  @override
+  Future<PageModel?> fetchPage(String slug) async {
+    try {
+      final jsonString = _sharedPreferences.getString(slug);
+
+      if (jsonString == null) throw Exception('Invalid response format');
+
+      final Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+      return PageModel.fromJson(jsonMap);
+    } catch (e) {
+      throw Exception('Failed to fetch page from local data source: $e');
+    }
+  }
+
+  @override
+  Future<void> storePage(PageModel page, String slug) async {
+    final String pageData = jsonEncode(page.toJson());
+
+    await _sharedPreferences.setString(slug, pageData);
+  }
+}
--- lib/features/public/pages/injection.dart
+++ lib/features/public/pages/injection.dart
@@ -1,22 +1,28 @@
 import 'package:pashboi/core/injection.dart';
 import 'package:pashboi/core/services/network/network_info.dart';
 import 'package:pashboi/core/services/network/product_api_service.dart';
+import 'package:pashboi/features/public/pages/data/data_sources/local_datasource.dart';
 import 'package:pashboi/features/public/pages/data/data_sources/remote_datasource.dart';
 import 'package:pashboi/features/public/pages/data/repositories/page_repository_impl.dart';
 import 'package:pashboi/features/public/pages/domain/repositories/page_repository.dart';
 import 'package:pashboi/features/public/pages/domain/usecases/fetch_page_usecase.dart';
 import 'package:pashboi/features/public/pages/presentation/bloc/page_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void registerMissionAndVisionModule() async {
+void registerPageModule() async {
   // Register Data Sources
   sl.registerLazySingleton<PageRemoteDataSource>(
     () => PageRemoteDataSourceImpl(productApiService: sl<ProductApiService>()),
+  );
+  sl.registerLazySingleton<PageLocalDataSource>(
+    () => PageLocalDataSourceImpl(sl<SharedPreferences>()),
   );
 
   // Register Repository
   sl.registerLazySingleton<PageRepository>(
     () => PageRepositoryImpl(
       pageRemoteDataSource: sl<PageRemoteDataSource>(),
+      pageLocalDataSource: sl<PageLocalDataSource>(),
       networkInfo: sl<NetworkInfo>(),
     ),
   );
@@ -27,5 +33,6 @@ void registerMissionAndVisionModule() async {
   );
 
   // Register Bloc
+
   sl.registerFactory<PageBloc>(() => PageBloc(pageUseCase: sl<PageUseCase>()));
 }
